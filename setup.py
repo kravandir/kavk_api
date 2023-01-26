0345d623d8f756bb1854c81085968d60a9dff0ba
Kravandir <kravandir@gmail.com>
2023-01-26 22:44:06 +0400

    add urls in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import find_packages, setup
 setup(
         name = 'avk_api',
         version='0.0.2',
+        url='https://github.com/kravandir/avk_api/',
+        download_url='https://github.com/kravandir/avk_api/archive/v{}.zip'.format('0.0.2'),
         package_dir={'': 'avk_api'},
         packages=find_packages('', include='avk_api'),
         description='Asynchronus and easy VK API',
